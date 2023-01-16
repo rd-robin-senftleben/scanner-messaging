ed55c156a97554f81b52f7ac1555180b16aef1e3
Robin Senftleben <66953865+rd-robin-senftleben@users.noreply.github.com>
2023-01-16 11:26:41 +0100

    Adds message types

--- pkg/message/messagetypes.go
+++ pkg/message/messagetypes.go
@@ -1,8 +1,14 @@
 package message
 
 const (
-	ASSET_MESSAGE_TYPE_ENUMERATION = "enumeration"
-	ASSET_MESSAGE_TYPE_PERMUTATION = "permutation"
+	ASSET_MESSAGE_TYPE_ENUMERATION      = "enumeration"
+	ASSET_MESSAGE_TYPE_ENUMERATION_DONE = "enumeration-done"
+	ASSET_MESSAGE_TYPE_PERMUTATION      = "permutation"
+	ASSET_MESSAGE_TYPE_PERMUTATION_DONE = "permutation-done"
+	ASSET_MESSAGE_TYPE_PORTSCAN         = "portscan"
+	ASSET_MESSAGE_TYPE_PORTSCAN_DONE    = "portscan-done"
+	ASSET_MESSAGE_TYPE_HTTPCONNECT      = "httpconnect"
+	ASSET_MESSAGE_TYPE_HTTPCONNECT_DONE = "httpconnect-done"
 )
 
 type AssetMessage struct {
